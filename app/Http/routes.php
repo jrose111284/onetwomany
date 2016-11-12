e38c6b75b925a87b56e30dc52f50a6e53164d86b
jamie rose <jrose111284@gmail.com>
2016-11-12 12:00:18 +0000

    full crud set up eith a one to many relationship

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,44 @@
 |
 */
 
+use App\User;
+use App\Post;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create', function () {
+   
+   $user = User::findOrFail(1);
+
+//    $post = new  Post(['title'=>'welcome', 'content'=> "glad you read this"]);
+
+    $user->posts()->save(new Post(['title'=>'welcome', 'body'=> "glad you read this"]));
+});
+
+
+Route::get('/read', function () {
+    $user = User::findOrFail(1);
+
+    foreach ($user->posts as $item ) {
+        echo $item->title . "<br>";
+    }
+
+});
+
+
+Route::get('/update', function () {
+    $user = User::findOrFail(1);
+git 
+//    $user->posts()->update(['title'=>'not welcome']);
+    $user->posts()->whereId(2)->update(['title'=>'hello welcome']);
+
+
+});
+
+Route::get('/delete', function () {
+    $user = User::findOrFail(1);
+
+    $user->posts()->whereId(2)->delete();
 });
